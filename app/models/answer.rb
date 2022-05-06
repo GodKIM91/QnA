8258eeaca3dcb026fff44644a323b2e39401b7b9
GodKIM91 <godkimovsk@gmail.com>
2022-05-06 11:55:28 +0300

    answer model formatting fix

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -8,9 +8,9 @@ class Answer < ApplicationRecord
 
   def set_as_best
     old_best_answer = question.answers.find_by(best: true)
-		transaction do
-			old_best_answer&.update!(best: false)
+    transaction do
+      old_best_answer&.update!(best: false)
       update!(best: true)
-		end
-	end
+    end
+  end
 end
