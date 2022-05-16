dad57e7e9f8d80af9324030e627dd97577e819d6
GodKIM91 <godkimovsk@gmail.com>
2022-05-16 22:51:21 +0300

    link model url validate and test

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe Link, type: :model do
 
   it { should validate_presence_of :name }
   it { should validate_presence_of :url }
+
+  it { should allow_value('https://github.com/').for(:url) }
+  it { should_not allow_value('something_wrong_url').for(:url) }
 end
 
 #rspec spec/models/link_spec.rb
