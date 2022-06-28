3c5795a282db86c471c4e5554e08ac8da1f3c764
GodKIM91 <godkimovsk@gmail.com>
2022-06-29 02:29:26 +0300

    specs

--- spec/jobs/new_answer_notify_job_spec.rb
+++ spec/jobs/new_answer_notify_job_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe NewAnswerNotifyJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:question) { create(:question, user: user) }
+  let(:answer) { create(:answer, question: question) }
+  let(:service) { double('NewAnswerService') }
+
+  before do
+    allow(NewAnswerService).to receive(:new).and_return(service)
+  end
+
+  it 'calls NewAnswerService#notify' do
+    expect(service).to receive(:notify).with(answer)
+    NewAnswerNotifyJob.perform_now(answer)
+  end
 end
+
+# rspec spec/jobs/new_answer_notify_job_spec.rb
--- spec/services/daily_digest_spec.rb
+++ spec/services/daily_digest_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 
 RSpec.describe DailyDigestService do
   let(:users) { create_list(:user, 3) }
-  it 'sends daily digest for all users' do
-    users.each { |user| expect(DailyDigestMailer).to receive(:digest).with(user).and_call_original }
+  let(:questions) { create_list(:question, 3) }
+
+
+  it 'sends daily digest to all users' do
+    new_questions = Question.where(created_at: Date.yesterday.all_day).to_a
+
+    users.each { |user| expect(DailyDigestMailer).to receive(:digest).with(user, new_questions).and_call_original }
     subject.send_digest
   end
 end
